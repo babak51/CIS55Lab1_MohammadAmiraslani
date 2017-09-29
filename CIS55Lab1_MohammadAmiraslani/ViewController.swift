2925bf7ac33b5bedb7dfc7abd1c96acf36369d59
Babak <baamira@yahoo.com>
2017-09-28 19:02:12 -0700

    adding text, label, and button

---
+++ CIS55Lab1_MohammadAmiraslani/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CIS55Lab1_MohammadAmiraslani
+//
+//  Created by Mohammad Amiraslani on 9/28/17.
+//  Copyright © 2017 Mohammad and Avi team CIS55. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
