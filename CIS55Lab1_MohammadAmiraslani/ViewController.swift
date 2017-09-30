74cd4576b7289620a056d3d40c12a2b9bf07adaf
Babak <baamira@yahoo.com>
2017-09-30 00:46:48 -0700

    adding changes Friday on 9/30/2017

--- CIS55Lab1_MohammadAmiraslani/ViewController.swift
+++ CIS55Lab1_MohammadAmiraslani/ViewController.swift
@@ -4,12 +4,16 @@
 //
 //  Created by Mohammad Amiraslani on 9/28/17.
 //  Copyright © 2017 Mohammad and Avi team CIS55. All rights reserved.
+//  GitHub: https//github.com/babak51/CIS55Lab1_MohammadAmiraslani.git
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    //MARK: Properties
+    @IBOutlet weak var myText: UITextField!
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,10 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    //MARK: Actions
 
-
+    @IBAction func moveTextToLabel(_ sender: UIButton) {
+        myLabel.text = myText.text
+        myText.text = ""
+    }
 }
-
